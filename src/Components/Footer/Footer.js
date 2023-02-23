ed9ddbd331cd04df448b7f1367ed7886e63e6919
Louis Perry <louis.perry3@gmail.com>
2023-02-23 08:16:13 -0800

    finished footer and header

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import "./Footer.css"
+import { CiFacebook, CiInstagram } from "react-icons/ci";
+import { CiTwitter } from "react-icons/ci";
 
 function Footer() {
   return (
     <div className='footer-container'>
-        <p>Privacy & Cookie Policies</p>
-        <div className='footerright'>
-            <p>2022</p>
-            <p><img src='assets.copyright.jpg'/>UniLife</p>
+        <div className='footertop'>
+            <div className='left'>
+                <h3>Keep in touch</h3>
+                <p className='textleft'>Curious about new offerings? Sign up for our weekly newsletter and stay informed.</p>
+                <input className="input" type={'email'} placeholder="Your email"/>
+
+            </div>
+            <div className='right'>
+                <h3>Let's Socialize</h3>
+                <a href='/' className='facebook'><CiFacebook/>Facebook</a>
+                <a href='/' className='twitter'><CiTwitter/>Twitter</a>
+                <a href='/' className='twitter'><CiInstagram/>Instagram</a>
+
+
+            </div>
         </div>
+        <div className='footerbottom'>
+            <div className='footerleft'>
+                <p>Terms & Conditions</p>
+                <p>Privacy & Cookie Policies</p>
+            </div>
+            <div className='footerright'>
+                <p>2022</p>
+                <p>&copy;UniLife</p>
+            </div>
         </div>
+     </div>
   )
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./Header.css"
+import logo from "../../assets/logo-img.png"
+import {BsHeart  } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 function Header() {
   return (
     <div className='Headercontainer'>
-        <a href='/' className='logoimg'><img src="assets.logo-img.jpg" 
+        <a href='/' className='logoimg'><img src={logo} 
         /> UniLife</a>
         <div className='shortlistcontact'>
-        <a href='/' className='shortlistimg'><img src="assets.shortlistheart.jpg" 
-        /> Shortlist</a>
-        <a href='/' className='contactimg'><img src="assets.mailimage.jpg" 
-        /> Contact Us</a>
+        <a href='/' className='shortlistimg'><BsHeart/>Shortlist</a>
+        <a href='/' className='contactimg'><FiMail/> Contact Us</a>
 
         </div>
         
